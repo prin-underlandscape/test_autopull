b836826353d303d0463852ae145c60fabacb8810
Augusto Ciuffoletti <augusto.ciuffoletti@gmail.com>
2024-07-09 11:54:34 +0200

    Aggiungo prova

--- script.py
+++ script.py
@@ -1,4 +1,14 @@
+
+import sys
+
 print("Hallo")
+
+
+for line in sys.stdin:
+    if 'Exit' == line.rstrip():
+        break
+    print(f'Processing Message from sys.stdin *****{line}*****')
+print("Done")
 
 with open('prova', 'r') as f:
     print(f.read())
